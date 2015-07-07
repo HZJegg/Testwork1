4404628ef7df6cdadb93c15fe7ad37db799ca79a
jenkins <hezhenjie@xiaoma.cn>
2015-07-07 16:11:10 +0800

    Initial Commit

---
+++ TestWork1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestWork1
+//
+//  Created by HZJ on 15/7/7.
+//  Copyright (c) 2015年 HZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestWork1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestWork1
+//
+//  Created by HZJ on 15/7/7.
+//  Copyright (c) 2015年 HZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
